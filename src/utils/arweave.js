f77b76e06654621152ab894ef5243c5c5e0ac720
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-05-11 18:48:06 +0300

    perf: chunks reseeding issue

--- src/utils/arweave.js
+++ src/utils/arweave.js
@@ -1,33 +1,42 @@
 import Arweave from "arweave";
-import { executeContract } from "@three-em/node";
-import { REDSTONE_GATEWAY_URL } from "./constants/gateway.js";
-import {
-  LoggerFactory,
-  SmartWeaveNodeFactory,
-} from "redstone-smartweave";
+import { SmartWeaveNodeFactory, LoggerFactory } from "redstone-smartweave";
 
 export const arweave = Arweave.init({
   host: "arweave.net",
   port: 443,
   protocol: "https",
-  timeout: 100000,
+  timeout: 60000,
   logging: false,
 });
 
+const smartweave = SmartWeaveNodeFactory.memCached(arweave);
 
-export async function readContract(contract_id) {
+export async function readContract(swc_id) {
+  try {
+    const contract = smartweave.contract(swc_id);
+    const res = await contract.readState();
 
-const sw = SmartWeaveNodeFactory.memCachedBased(arweave)
-    .useRedStoneGateway()
-    .build();
-
-  const contract = sw.contract(contract_id).setEvaluationOptions({
-      allowUnsafeClient: true,
-      ignoreExceptions: true
-    });
-
-  const { state, validity } = await contract.readState();
-
-  return state;
-
+    return res.state;
+  } catch (error) {
+    console.log(error);
+  }
 }
+
+// redstone-smartweave@0.4.23
+// const sw = SmartWeaveNodeFactory.memCachedBased(arweave)
+// export async function readContract(contract_id) {
+
+// const sw = SmartWeaveNodeFactory.memCachedBased(arweave)
+//     .useRedStoneGateway()
+//     .build();
+
+//   const contract = sw.contract(contract_id).setEvaluationOptions({
+//       allowUnsafeClient: true,
+//       ignoreExceptions: true
+//     });
+
+//   const { state, validity } = await contract.readState();
+
+//   return state;
+
+// }
