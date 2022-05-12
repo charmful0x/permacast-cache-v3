e9b7810fb571d71f7e5d733dae37b4debd879e22
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-05-13 02:09:31 +0300

    fix: a new V3 SRC

--- src/utils/constants/v2_v3_conversion.js
+++ src/utils/constants/v2_v3_conversion.js
@@ -1,92 +1,180 @@
-export const V2_V3_ARRAY = [
+export const V2_V3_ARRAY =[
   {
     old: "ELTf8GqoDaEww9zd63XKW3x0UrM1aNbJWEGtIgpypnE",
-    new: "fSpJkjkgLno1Oco9ZZI7ADIklcKVU9YShU15PvKqgNg",
+    new: "uscmie9LmBtl0qjVa8w2gKAlupaMLQV7jGrAQgokUzg",
     owner: "JTFaAFT5OHSgPidW-4zdMQNQKwj-jXUQKTHgGECQ3eY",
   },
   {
     old: "mshYBN4mzf7PAVWYpOw6u97iONnwQN3Ivl-R94070z8",
-    new: "z5agZbaunO-_fVfSj8EHnOSNEQfrS7ni50-kPwZ2Qqs",
+    new: "K_d9iBJdXSMfekG_jHEGBhONtrvt_CtyA22hsXej588",
     owner: "wnrOZuhcf3Tsc3uktpXErcVBYYYADG3wFOk6yJq9xyY",
   },
   {
     old: "nGiHqQVBY9Ag4DPq6tJegTUFYa3e6Y28fvsiiIuS0Sg",
-    new: "zWJxaX_GcB8R0N0dD73ICKMTkAq4-XY1lq53hE5b2UA",
+    new: "uRgSe7Axy43x38l6XIgxXMXjhAEC9W-iOLeJuJ1Sexk",
     owner: "YAPPi1QIaK4IrV4LuINrysoQYTpqjmpp-memABm_jhc",
   },
   {
     old: "e5sFnrHDzmGZ_5lc8vucpnwEJEaKC_xvLMsypacBggk",
-    new: "WuyKjdD3hX0FTZJiahmVSkxt3GU8c_bwMYtkjCvgKQE",
+    new: "3-hzqR63NQCBDX_Swrup-4or3qTVCsbI4BrfWHUNAFI",
     owner: "O81Ukzu0bK-JrDVSpIqMTJHU3DWWz01VVO-oYchyzm4",
   },
   {
     old: "OPz96SB2B2GI7KGFfsA94GXwy4hBrxjDwXazezKrEFw",
-    new: "QJC90cDp_k08sn5terL8o0m3mByl0mVKNpYHL2x996A",
+    new: "12swdcum5JnnyDXmRHmiWyhu9aeldxwCSYBTw1aiKl0",
     owner: "xLGhOYRlC2Somw9tDRaDF6o8qtJWAjS8IRsgmKiADww",
   },
   {
     old: "5tOaFJHXSTOs4DvZDPJ49aeTBxe1x2uRnd7JWheQubM",
-    new: "DedgomL_Rq4Asyu5uMfLUNkXh_iHnZr9QtIqdqccc54",
+    new: "KFLdrdmrKN4hqKKurZh32LUVOFSMDYp9BSLrcHOabtY",
     owner: "hctnleYoGzMf55h3C4pnG3luEUElXkvJj7XWSv7NpUM",
   },
   {
     old: "wpXYXfSuFUe_IjhLTPwZbeKo556PkQYetdozKLlghjs",
-    new: "76EF-xZCNsE2zoLYLUPKLyAmGOyWmxBKn0Ffc8Hqsp0",
+    new: "YCc9Z5XhN2D4fj0o82kR6h0NHL_Eavv6UJpA_LIQXcg",
     owner: "iJJzN3WROxxSZK7bEdsap4rsSDHbD6xKx43RAXyc8eM",
   },
   {
     old: "Cciamj3Xc6kFKShphi83ziUZus9OsdHp1u99Ovfm9Lg",
-    new: "BNdbjqe-PPsjRhrmffhY80Kn-BzWiBH408ZjrjSiJ5M",
+    new: "42VDGVmJJ_PMIzrCZO766kShYT-M4kYb2nsmIAUmwNA",
     owner: "t8i_MHAPl-mwDSthhyBAwGWGrI-_foX5SZV_h9f6Vrc",
   },
   {
     old: "IsnhHYs1JXLBBIwFgz8ztnBvcR6dpFVkJM_YRHIXJfE",
-    new: "3JHVDKE1EPFFKUksL388Tfa6IxRo8WW83BySpc-bkSg",
+    new: "A2lsXRyENOpbjeGebxxzo4oxMvd8MFCQo5PriszSrNQ",
     owner: "ZW_Caoamf7WtLKFlKLA3lypS0Oa3eDzkL64bInW1Mio",
   },
   {
     old: "M2-SoG0808-4ap98pcU_meqXWhniSQk2KGJoir-7pDw",
-    new: "kctqtRnY0_yoKtcNPCVcvVQuM_-a-53OfmYOMWzXJAw",
+    new: "B21pfbKN9PCVpXz7n-af7BfoJ2jsLt2QmIy0c5spHUo",
     owner: "i_0lXhTIsKKABEKYNlNTiKtY3QTbo9Dm56uzEvvi85g",
   },
   {
     old: "bVdFsVa1AxCWgt6i_0zMS5OxmVJmzsANF-izwCQcnh4",
-    new: "FyPHCEGS-oaD_HU8rQMPjU3ELoX72CbFgLdPVm96B_Y",
+    new: "o7ay0TuNEQeUJgwUYYtDU5vgN_PW0BSEbE9MP_8xD6Y",
     owner: "MPxAQWxRl6KwsnAmGeciWNjIiplsqByeiRMLyFIKF-k",
   },
   {
     old: "d8D6LyEoHBD9-y9KlixAQwfwcSW7XQt_qHZMu0RbLOI",
-    new: "pkf1KAOck4cmx26Wb8g1EDGs82Acvirx495xVFnBzh4",
+    new: "eJPyUOesHaxkfhzsxzFbc4HU7r4rXj901gYfHelMtEo",
     owner: "qOcc9Bc-afSzzCg8pYs5fHe7qxxSO8F5XqrEEnaJUgM",
   },
   {
     old: "8upJILbuOURWTDkNtTZRoqmF32J2zjsw4PGj3SNWZ40",
-    new: "2btKdMyMegcTtQk_npg8F2NPlPh25okc6DJAK-wkutQ",
-    owner: "AeK_9yb3f3HEK1Gzwky6tIx8ujW9Pxr_FkhCkWftFtw",
-  },
-  {
-    old: "Ym2i1XWBAuabbw0qcmSSsmjGtBJbJlDLDxxf277XtK8",
-    new: "YdRT_-76No36Us5Im01gzOnJc5VKqxrJgicx0vY5erg",
+    new: "aW4pbR02td6e4j456a62GeWIisQe-a8IQb0zrpG4tWk",
     owner: "AeK_9yb3f3HEK1Gzwky6tIx8ujW9Pxr_FkhCkWftFtw",
   },
   {
     old: "ZjHDtQQl-LuN090S6ZkCHi3Av6Z4xfxIeP_FC-PatM8",
-    new: "fbbsAMZKO1Rwv83KCE7kOqTgpR5REJJlShwQUR61E4s",
+    new: "oGg4E56sSE1_Ld-VVdTkNQLWT5aM5iGiLNVXL7J8bCU",
     owner: "_r-2mK0YP-yNoXbbmALn36v2F6hyqx4Gjo3ZP_FyMBg",
   },
   {
     old: "nMZbYGB55uFAPorP8IoI0LduBkie2gItRCNDRTnh1XA",
-    new: "Q5MjAln3NWpI2JKVLWk-mT8KoEa7DMQi-6Lieg6_how",
+    new: "k70iBkQn1Kru2qs0_gRwIcTu2h7gtEMSne4HHRIQ9GQ",
     owner: "qBRa782iJ4Nry0apjK0svfIJK-X_7mppep7bnDOH6_g",
   },
   {
     old: "K3vHfTvvl5Ca4qHKAKzzfmxMvkB5oNSibkzqo_h5_HI",
-    new: "V0GG9sEfVmjHcNo4Mhf_Mht-zX_VtsnlwoiZ2tZHQo8",
+    new: "5S3h6qiKX63MBoIdOHMX-da7I23Enso6N0VLbIJI_rg",
     owner: "u3cs3x6tvytcR6rv3JgoCK8t1dsIJyaf_Rx7MJzxc-I",
   },
   {
     old: "cO1rSCN3RpQUYwzl4j9sr881wdlqDD7FsrJHjYbAztQ",
-    new: "0Wvsnn91BkiGztZ3gutH-g0DC6aFYrEDArprMB3Dl2Q",
+    new: "_mF85KX4KgKEfb6PF8fQu2LNXgH3az0U1M6JVTq38b4",
     owner: "kaYP9bJtpqON8Kyy3RbqnqdtDBDUsPTQTNUCvZtKiFI",
   },
 ];
+
+// export const V2_V3_ARRAY = [
+//   {
+//     old: "ELTf8GqoDaEww9zd63XKW3x0UrM1aNbJWEGtIgpypnE",
+//     new: "fSpJkjkgLno1Oco9ZZI7ADIklcKVU9YShU15PvKqgNg",
+//     owner: "JTFaAFT5OHSgPidW-4zdMQNQKwj-jXUQKTHgGECQ3eY",
+//   },
+//   {
+//     old: "mshYBN4mzf7PAVWYpOw6u97iONnwQN3Ivl-R94070z8",
+//     new: "z5agZbaunO-_fVfSj8EHnOSNEQfrS7ni50-kPwZ2Qqs",
+//     owner: "wnrOZuhcf3Tsc3uktpXErcVBYYYADG3wFOk6yJq9xyY",
+//   },
+//   {
+//     old: "nGiHqQVBY9Ag4DPq6tJegTUFYa3e6Y28fvsiiIuS0Sg",
+//     new: "zWJxaX_GcB8R0N0dD73ICKMTkAq4-XY1lq53hE5b2UA",
+//     owner: "YAPPi1QIaK4IrV4LuINrysoQYTpqjmpp-memABm_jhc",
+//   },
+//   {
+//     old: "e5sFnrHDzmGZ_5lc8vucpnwEJEaKC_xvLMsypacBggk",
+//     new: "WuyKjdD3hX0FTZJiahmVSkxt3GU8c_bwMYtkjCvgKQE",
+//     owner: "O81Ukzu0bK-JrDVSpIqMTJHU3DWWz01VVO-oYchyzm4",
+//   },
+//   {
+//     old: "OPz96SB2B2GI7KGFfsA94GXwy4hBrxjDwXazezKrEFw",
+//     new: "QJC90cDp_k08sn5terL8o0m3mByl0mVKNpYHL2x996A",
+//     owner: "xLGhOYRlC2Somw9tDRaDF6o8qtJWAjS8IRsgmKiADww",
+//   },
+//   {
+//     old: "5tOaFJHXSTOs4DvZDPJ49aeTBxe1x2uRnd7JWheQubM",
+//     new: "DedgomL_Rq4Asyu5uMfLUNkXh_iHnZr9QtIqdqccc54",
+//     owner: "hctnleYoGzMf55h3C4pnG3luEUElXkvJj7XWSv7NpUM",
+//   },
+//   {
+//     old: "wpXYXfSuFUe_IjhLTPwZbeKo556PkQYetdozKLlghjs",
+//     new: "76EF-xZCNsE2zoLYLUPKLyAmGOyWmxBKn0Ffc8Hqsp0",
+//     owner: "iJJzN3WROxxSZK7bEdsap4rsSDHbD6xKx43RAXyc8eM",
+//   },
+//   {
+//     old: "Cciamj3Xc6kFKShphi83ziUZus9OsdHp1u99Ovfm9Lg",
+//     new: "BNdbjqe-PPsjRhrmffhY80Kn-BzWiBH408ZjrjSiJ5M",
+//     owner: "t8i_MHAPl-mwDSthhyBAwGWGrI-_foX5SZV_h9f6Vrc",
+//   },
+//   {
+//     old: "IsnhHYs1JXLBBIwFgz8ztnBvcR6dpFVkJM_YRHIXJfE",
+//     new: "3JHVDKE1EPFFKUksL388Tfa6IxRo8WW83BySpc-bkSg",
+//     owner: "ZW_Caoamf7WtLKFlKLA3lypS0Oa3eDzkL64bInW1Mio",
+//   },
+//   {
+//     old: "M2-SoG0808-4ap98pcU_meqXWhniSQk2KGJoir-7pDw",
+//     new: "kctqtRnY0_yoKtcNPCVcvVQuM_-a-53OfmYOMWzXJAw",
+//     owner: "i_0lXhTIsKKABEKYNlNTiKtY3QTbo9Dm56uzEvvi85g",
+//   },
+//   {
+//     old: "bVdFsVa1AxCWgt6i_0zMS5OxmVJmzsANF-izwCQcnh4",
+//     new: "FyPHCEGS-oaD_HU8rQMPjU3ELoX72CbFgLdPVm96B_Y",
+//     owner: "MPxAQWxRl6KwsnAmGeciWNjIiplsqByeiRMLyFIKF-k",
+//   },
+//   {
+//     old: "d8D6LyEoHBD9-y9KlixAQwfwcSW7XQt_qHZMu0RbLOI",
+//     new: "pkf1KAOck4cmx26Wb8g1EDGs82Acvirx495xVFnBzh4",
+//     owner: "qOcc9Bc-afSzzCg8pYs5fHe7qxxSO8F5XqrEEnaJUgM",
+//   },
+//   {
+//     old: "8upJILbuOURWTDkNtTZRoqmF32J2zjsw4PGj3SNWZ40",
+//     new: "2btKdMyMegcTtQk_npg8F2NPlPh25okc6DJAK-wkutQ",
+//     owner: "AeK_9yb3f3HEK1Gzwky6tIx8ujW9Pxr_FkhCkWftFtw",
+//   },
+//   {
+//     old: "Ym2i1XWBAuabbw0qcmSSsmjGtBJbJlDLDxxf277XtK8",
+//     new: "YdRT_-76No36Us5Im01gzOnJc5VKqxrJgicx0vY5erg",
+//     owner: "AeK_9yb3f3HEK1Gzwky6tIx8ujW9Pxr_FkhCkWftFtw",
+//   },
+//   {
+//     old: "ZjHDtQQl-LuN090S6ZkCHi3Av6Z4xfxIeP_FC-PatM8",
+//     new: "fbbsAMZKO1Rwv83KCE7kOqTgpR5REJJlShwQUR61E4s",
+//     owner: "_r-2mK0YP-yNoXbbmALn36v2F6hyqx4Gjo3ZP_FyMBg",
+//   },
+//   {
+//     old: "nMZbYGB55uFAPorP8IoI0LduBkie2gItRCNDRTnh1XA",
+//     new: "Q5MjAln3NWpI2JKVLWk-mT8KoEa7DMQi-6Lieg6_how",
+//     owner: "qBRa782iJ4Nry0apjK0svfIJK-X_7mppep7bnDOH6_g",
+//   },
+//   {
+//     old: "K3vHfTvvl5Ca4qHKAKzzfmxMvkB5oNSibkzqo_h5_HI",
+//     new: "V0GG9sEfVmjHcNo4Mhf_Mht-zX_VtsnlwoiZ2tZHQo8",
+//     owner: "u3cs3x6tvytcR6rv3JgoCK8t1dsIJyaf_Rx7MJzxc-I",
+//   },
+//   {
+//     old: "cO1rSCN3RpQUYwzl4j9sr881wdlqDD7FsrJHjYbAztQ",
+//     new: "0Wvsnn91BkiGztZ3gutH-g0DC6aFYrEDArprMB3Dl2Q",
+//     owner: "kaYP9bJtpqON8Kyy3RbqnqdtDBDUsPTQTNUCvZtKiFI",
+//   },
+// ];
