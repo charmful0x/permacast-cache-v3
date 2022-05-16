558f1973d38459cd97b297802e4df7436310b6e2
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-05-17 01:42:43 +0300

    fix: remove a fixed factory

--- src/utils/cache.js
+++ src/utils/cache.js
@@ -30,7 +30,6 @@ async function removeBlacklists(podObj) {
     [
       "8upJILbuOURWTDkNtTZRoqmF32J2zjsw4PGj3SNWZ40",
       "IsnhHYs1JXLBBIwFgz8ztnBvcR6dpFVkJM_YRHIXJfE",
-      "K3vHfTvvl5Ca4qHKAKzzfmxMvkB5oNSibkzqo_h5_HI",
       "bVdFsVa1AxCWgt6i_0zMS5OxmVJmzsANF-izwCQcnh4",
     ].includes(podObj.childOf)
   ) {
